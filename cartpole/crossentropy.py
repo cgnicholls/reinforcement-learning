b685b635d941d3de39848526a01e19f4b881fd0a
Chris Nicholls <cgnicholls@googlemail.com>
2016-08-20 22:33:06 +0100

    Changed crossentropy to be stochastic again, but with initial_variance as a parameter -- this makes it more deterministic

--- cartpole/crossentropy.py
+++ cartpole/crossentropy.py
@@ -12,44 +12,39 @@ env = gym.make('CartPole-v0')
 # num_episodes: the number of episodes to run
 # max_episode_length: a cap on the maximum length of an episode
 # theta: the parameter to use for the policy
-# use_stochastic_policy: if false, then we move right if x dot theta > 0, and
-# left otherwise; if true, then we move right with probability sigmoid(x dot
-# theta), and move left otherwise. You get much better results by NOT using a
-# stochastic policy
-def cross_entropy_agent(num_episodes, max_episode_length, theta,
-        use_stochastic_policy=False):
+def cross_entropy_agent(num_episodes, max_episode_length, theta):
     rewards = []
     for i_episode in xrange(num_episodes):
         episode_reward = 0
         observation = env.reset()
         for t in xrange(max_episode_length):
             env.render()
-            action = sample_action(observation, theta, use_stochastic_policy)
+            action = sample_action(observation, theta)
             observation, reward, done, info = env.step(action)
             episode_reward += reward
             if done:
                 rewards.append(episode_reward)
-                print("Reward for episode: {t}".format(episode_reward))
+                print("Reward for episode: {}".format(episode_reward))
                 break
 
 # Train the cross entropy agent.
-# num_iterations: the number of iterations to use the method
-# num_samples: the number of samples to take of theta
-# elite_fraction: the fraction of samples to take the elite set from
-# use_stochastic_policy: if false, then we move right if x dot theta > 0, and
-# left otherwise; if true, then we move right with probability sigmoid(x dot
-# theta), and move left otherwise. You get much better results by NOT using a
-# stochastic policy
-# num_iters_for_estimate: the number of iterations to run the estimate of the
+# - max_iterations: the maximum number of iterations to use the method
+# - num_samples: the number of samples to take of theta
+# - elite_fraction: the fraction of samples to take the elite set from
+# - reward_goal: exit the function when we reach this average reward
+# - initial_variance: the initial variance for the distribution -- increase this
+# to encourage the samples, theta, to be larger in magnitude, and thus be more
+# deterministic.
+# - num_iters_for_estimate: the number of iterations to run the estimate of the
 # reward for each parameter vector
-# max_episode_length: a cap on the maximum length of an episode
-def train_cross_entropy_agent(num_iterations, num_samples, elite_fraction,
-        use_stochastic_policy=False, verbose=True, num_iters_for_estimate=100,
-        max_episode_length=5000):
+# - max_episode_length: a cap on the maximum length of an episode
+def train_cross_entropy_agent(max_iterations, num_samples, elite_fraction,
+        reward_goal=2000, verbose=True, initial_variance=10,
+        num_iters_for_estimate=100, max_episode_length=5000):
     # Initialise mu and sigma2.
-    mu = [0,0,0,0]
-    sigma2 = [1,1,1,1]
-    for iter in xrange(num_iterations):
+    mu = [0] * 4
+    sigma2 = [initial_variance] * 4
+    for iter in xrange(max_iterations):
         print("Iteration: {}".format(iter))
         sample_theta = []
         reward_estimates = []
@@ -59,12 +54,16 @@ def train_cross_entropy_agent(num_iterations, num_samples, elite_fraction,
 
             # Estimate the reward for this theta
             reward_estimates.append(estimate_reward_with_theta(sample_theta[j],
-                    num_iters_for_estimate, max_episode_length,
-                    use_stochastic_policy))
+                    num_iters_for_estimate, max_episode_length))
 
         # Print the average reward
+        average_reward = np.mean(reward_estimates)
         if verbose:
-            print("Average reward: {}".format(np.mean(reward_estimates)))
+            print("Average reward: {}".format(average_reward))
+
+        # If our average reward is at least the reward goal, then we are done
+        if average_reward >= reward_goal:
+            break
 
         # Now keep the top elite_fraction fraction of parameters theta, as
         # measured by reward
@@ -92,14 +91,13 @@ def compute_elite_set(sample_theta, reward_estimates, elite_fraction):
 # theta: the parameter to use
 # num_iters: the number of iterations to use to compute the average
 # max_episode_length: the maximum episode length to consider
-def estimate_reward_with_theta(theta, num_iters, max_episode_length,
-        use_stochastic_policy):
+def estimate_reward_with_theta(theta, num_iters, max_episode_length):
     rewards = []
     for i in xrange(num_iters):
         episode_reward = 0
         observation = env.reset()
         for t in xrange(max_episode_length):
-            action = sample_action(observation, theta, use_stochastic_policy)
+            action = sample_action(observation, theta)
             observation, reward, done, info = env.step(action)
             episode_reward += reward
             if done:
@@ -116,19 +114,13 @@ def sample_from_gaussian(mu, sigma2):
 # theta: the parameter vector theta
 # Returns: a sample from the policy distribution. The distribution is: move
 # right with probability sigma(x dot theta), and otherwise move left.
-def sample_action(observation, theta, stochastic):
-    if stochastic:
-        prob_move_right = sigmoid(np.dot(observation, np.transpose(theta)))
-        r = np.random.rand()
-        if r < prob_move_right:
-            return 1
-        else:
-            return 0
+def sample_action(observation, theta):
+    prob_move_right = sigmoid(np.dot(observation, np.transpose(theta)))
+    r = np.random.rand()
+    if r < prob_move_right:
+        return 1
     else:
-        if np.dot(observation, np.transpose(theta)) > 0:
-            return 1
-        else:
-            return 0
+        return 0
 
 # Computes the sigmoid function
 # u: a real number
@@ -145,10 +137,9 @@ def fit_gaussian_to_samples(samples):
     sigma2 = np.var(samples,0)
     return [mu, sigma2]
 
-# Train the cross entropy agent on 5 iterations of the cross entropy method,
-# using 100 samples each time, and only keeping the top 10%. We don't use a
-# stochastic policy.
-trained_theta = train_cross_entropy_agent(5, 100, 0.1, False)
+# Train the agent using at most 100 iterations of the cross entropy method,
+# using 100 samples each time, and only keeping the top 10%.
+trained_theta = train_cross_entropy_agent(100, 100, 0.1)
 
 # Run the agent with our computed theta
 cross_entropy_agent(10, 10000, trained_theta)
