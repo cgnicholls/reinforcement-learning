376d989122acf351f4676446ab3d1f527145470d
Chris Nicholls <cgnicholls@googlemail.com>
2017-03-25 15:08:24 +0000

    Added command line options to a3c

--- a3c/a3c.py
+++ a3c/a3c.py
@@ -2,7 +2,7 @@
 # Implements the asynchronous advantage actor-critic algorithm.
 
 import os
-import sys
+import sys, getopt
 import multiprocessing
 import threading
 import tensorflow as tf
@@ -20,17 +20,16 @@ random.seed(100)
 # FLAGS
 T_MAX = 100000000
 NUM_THREADS = 8
-STATE_FRAMES = 4
 INITIAL_LEARNING_RATE = 1e-4
 DISCOUNT_FACTOR = 0.99
-VERBOSE_EVERY = 50000
+VERBOSE_EVERY = 10000
 TESTING = False
 
 I_ASYNC_UPDATE = 5
-FLAGS = {"T_MAX": T_MAX, "NUM_THREADS": NUM_THREADS, "STATE_FRAMES":
-STATE_FRAMES, "INITIAL_LEARNING_RATE": INITIAL_LEARNING_RATE, "DISCOUNT_FACTOR":
-DISCOUNT_FACTOR, "VERBOSE_EVERY": VERBOSE_EVERY, "TESTING": TESTING,
-"I_ASYNC_UPDATE": I_ASYNC_UPDATE}
+
+FLAGS = {"T_MAX": T_MAX, "NUM_THREADS": NUM_THREADS, "INITIAL_LEARNING_RATE":
+INITIAL_LEARNING_RATE, "DISCOUNT_FACTOR": DISCOUNT_FACTOR, "VERBOSE_EVERY":
+VERBOSE_EVERY, "TESTING": TESTING, "I_ASYNC_UPDATE": I_ASYNC_UPDATE}
 
 training_finished = False
 
@@ -58,7 +57,7 @@ class Summary:
         self.writer.add_summary(summary_to_add, global_step=t)
 
 def async_trainer(agent, env, sess, thread_idx, T_queue, summary, saver,
-    checkpoint_file):
+    save_path):
     print "Training thread", thread_idx
     T = T_queue.get()
     T_queue.put(T+1)
@@ -167,7 +166,7 @@ def estimate_reward(agent, env, episodes=10, max_steps=10000):
         episode_rewards.append(episode_reward)
     return episode_rewards, episode_vals
 
-def evaluator(agent, env, sess, T_queue, summary, saver, checkpoint_file):
+def evaluator(agent, env, sess, T_queue, summary, saver, save_path):
     # Read T and put the same T back on.
     T = T_queue.get()
     T_queue.put(T)
@@ -190,13 +189,13 @@ def evaluator(agent, env, sess, T_queue, summary, saver, checkpoint_file):
             print "Avg ep reward", avg_ep_r, "Average value", avg_val
 
             summary.write_summary({'episode_avg_reward': avg_ep_r, 'avg_value': avg_val}, T)
-            saver.save(sess, checkpoint_file, global_step=T)
-            print "Saved"
+            checkpoint_file = saver.save(sess, save_path, global_step=T)
+            print "Saved in", checkpoint_file
         sleep(1.0)
 
 # If restore is True, then start the model from the most recent checkpoint.
 # Else initialise as usual.
-def a3c(game_name, num_threads=8, restore=False, checkpoint_file='model'):
+def a3c(game_name, num_threads=8, restore=None, save_path='model'):
     processes = []
     envs = []
     for _ in range(num_threads+1):
@@ -212,9 +211,6 @@ def a3c(game_name, num_threads=8, restore=False, checkpoint_file='model'):
     evaluation_env = envs[0]
     envs = envs[1:]
 
-    T_queue = Queue.Queue()
-    T_queue.put(0)
-
     with tf.Session() as sess:
         agent = Agent(session=sess, observation_shape=envs[0].observation_shape,
         action_size=envs[0].action_size,
@@ -223,21 +219,28 @@ def a3c(game_name, num_threads=8, restore=False, checkpoint_file='model'):
         # Create a saver, and only keep 2 checkpoints.
         saver = tf.train.Saver(max_to_keep=2)
 
-        if restore:
-            saver.restore(sess, checkpoint_file)
+        T_queue = Queue.Queue()
+
+        # Either restore the parameters or don't.
+        if restore is not None:
+            saver.restore(sess, save_path + '-' + str(restore))
+            last_T = restore
+            print "T was:", last_T
+            T_queue.put(last_T)
         else:
             sess.run(tf.global_variables_initializer())
+            T_queue.put(0)
 
-        summary = Summary('tensorboard', agent)
+        summary = Summary(save_path, agent)
 
         # Create a process for each worker
         for i in range(num_threads):
             processes.append(threading.Thread(target=async_trainer, args=(agent,
-            envs[i], sess, i, T_queue, summary, saver, checkpoint_file,)))
+            envs[i], sess, i, T_queue, summary, saver, save_path,)))
 
         # Create a process to evaluate the agent
         processes.append(threading.Thread(target=evaluator, args=(agent,
-        evaluation_env, sess, T_queue, summary, saver, checkpoint_file,)))
+        evaluation_env, sess, T_queue, summary, saver, save_path,)))
 
         # Start all the processes
         for p in processes:
@@ -259,24 +262,45 @@ def discount(rewards, gamma):
 def test_equals(arr1, arr2, eps):
     return np.sum(np.abs(np.array(arr1)-np.array(arr2))) < eps
 
-if len(sys.argv) == 2:
-    game_name = sys.argv[1]
-    print "Using game", game_name
-    checkpoint_file = 'model/' + game_name + '-model-' + \
-    strftime("%d-%m-%Y-%H:%M:%S", gmtime())
-    restore = False
-elif len(sys.argv) == 3:
-    # Eventually want to be able to restore, but can't currently.
-    game_name = sys.argv[1]
-    checkpoint_file = sys.argv[2]
-    restore = True
-else:
+def main(argv):
+    num_threads = NUM_THREADS
     game_name = 'SpaceInvaders-v0'
-    checkpoint_file = 'model/' + game_name + '-model-' + \
-    strftime("%d-%m-%Y-%H:%M:%S", gmtime())
-    restore = False
+    restore = None
+    try:
+        opts, args = getopt.getopt(argv, "hg:s:r:t:")
+    except getopt.GetoptError:
+        print "To run the OpenAI Gym game and save to the given save path: \
+        a3c.py -g <game name> -s <save path> -t <num threads>"
+        print "To resume training playing on the given OpenAI Gym game from \
+        the given save path: python a3c.py -g <game name> -s <save path> -t \
+        <num threads> -r <the T in save_path-T>."
+    for opt, arg in opts:
+        if opt == '-h':
+            print "Options: -g <game name>, -s <save path>, -r (restore from \
+            the save path given), -t <num threads>."
+            sys.exit()
+        elif opt == '-g':
+            game_name = arg
+        elif opt == '-s':
+            save_path = arg
+        elif opt == '-r':
+            restore = int(arg)
+        elif opt == '-t':
+            num_threads = int(arg)
+            print "Using", num_threads, "threads."
+    if game_name is None:
+        print "No game name specified, so playing", game_name
+    if save_path is None:
+        save_path = 'model/' + game_name + '-model-' + \
+        strftime("%d-%m-%Y-%H:%M:%S/", gmtime())
+        print "No save path specified, so saving to", save_path
+    if not os.path.exists(save_path):
+        print "Path doesn't exist, so creating"
+        os.makedirs(save_path)
+    print "Using save path", save_path
+    print "Using flags", FLAGS
+    a3c(game_name, num_threads=NUM_THREADS, restore=restore,
+    save_path=save_path)
 
-print "Using flags", FLAGS
-print "Using checkpoint file", checkpoint_file
-a3c(game_name, num_threads=NUM_THREADS, restore=restore,
-checkpoint_file=checkpoint_file)
+if __name__ == "__main__":
+    main(sys.argv[1:])
